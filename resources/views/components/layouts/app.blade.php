a9751e002215c869c206e23a01f82c9a9e15ec68
Dhruv <24amtics067@gmail.com>
2025-12-16 20:19:39 +0530

    Fixed issue where livewire was not running it's js script while being present in final output due to asset(), fixed by putting alpine js script with asset() at end of body rather than head tag

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,13 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- <link rel="stylesheet" href="public/build/assets/app-DlbDCAyn.css"> -->
-        @vite(["resources/css/app.css"])
-        <script src="public/build/assets/app-CJy8ASEk.js"></script>
+        <link rel="stylesheet" href="{{ asset('build/assets/app-BVdCxMie.css') }}">
+        <!-- <script src="build/assets/app-CJy8ASEk.js"></script> -->
 
         <title>{{ $title ?? config('app.name') }}</title>
     </head>
     <body>
         {{ $slot }}
+        <script src="{{ asset('build/assets/app-CJy8ASEk.js') }}"></script>
     </body>
 </html>
